30aab183175a194690d6964f5ac98906dac0ca1f
Geoff P <gpamerleau@gmail.com>
2020-09-01 06:11:42 -0600

    Updated

--- userdata.sh
+++ userdata.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 apt update && apt upgrade -y
 git clone https://github.com/Sy14r/SecListsCombined.git
+rm -f SecListsCombined/COMBINED-PASS.txt
 chmod +x ./SecListsCombined/build.sh
 ./SecListsCombined/build.sh
