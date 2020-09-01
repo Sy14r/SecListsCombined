23c4888cdb55af757b92e014a7a588635005eef4
Geoff P <gpamerleau@gmail.com>
2020-09-01 08:14:17 -0600

    Updating...

--- userdata.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-apt update && apt upgrade -y && apt install -y binutils
-git clone https://github.com/Sy14r/SecListsCombined.git
-rm -f SecListsCombined/COMBINED-PASS.txt
-chmod +x ./SecListsCombined/build.sh
-./SecListsCombined/build.sh
